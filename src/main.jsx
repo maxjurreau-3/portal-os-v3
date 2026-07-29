693fe11da5abc7bb923f742fdc2767e0c6c77afc
maxjurreau-3 <maxjurreau@gmail.com>
2026-07-29 19:23:44 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,5 @@
+import { enableHotModuleReload } from "./runtime/hmr.js";
+enableHotModuleReload();
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { PortalShell } from "./runtime/shell.jsx";
