8f82001b1842f5381aa50381ae85863f187036ed
maxjurreau-3 <maxjurreau@gmail.com>
2026-07-29 19:19:01 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import { bootPortalOS } from "./runtime/boot.jsx";
-
-bootPortalOS();
+import { PortalShell } from "./runtime/shell.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <PortalShell />
 );
